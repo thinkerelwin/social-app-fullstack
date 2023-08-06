0f9aebff378a0809c7702d901425491a87c343ea
thinkerelwin <thinkerelwin@gmail.com>
2023-08-06 22:08:32 +0800

    add the rest of API for users in controllers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -34,3 +34,57 @@ export async function getUserFriends(req, res) {
     res.status(404).json({ message: error.message });
   }
 }
+
+export async function addFriends(req, res) {
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+
+    if (!user.friends.includes(friendId)) {
+      user.friends.push(friendId);
+      friend.friends.push(id);
+
+      await user.save();
+      await friend.save();
+
+      const friends = await Promise.all(
+        user.friends.map((friendId) => User.findById(friendId))
+      );
+
+      const formattedFriends = friends.map((friend) => ({
+        _id: friend._id,
+        firstName: friend.firstName,
+        lastName: friend.lastName,
+        occupation: friend.occupation,
+        location: friend.location,
+        picturePath: friend.picturePath,
+      }));
+
+      res.status(200).json(formattedFriends);
+    } else {
+      throw new Error(
+        "The user you want to add is already on your friend list"
+      );
+    }
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
+export async function removeFriends(req, res) {
+  try {
+    const { id: userId, friendId } = req.params;
+    const user = await User.findById(userId);
+    const friend = await User.findById(friendId);
+
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== userId);
+    } else {
+      throw new Error("The user you want to remove isn't on your friend list");
+    }
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
