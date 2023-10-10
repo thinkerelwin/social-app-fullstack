676b5481f0cba81036267446a9f0f246f2ff2cc5
thinkerelwin <thinkerelwin@gmail.com>
2023-10-10 23:57:46 +0800

    add test container file

---
+++ client/cypress/utils/TestContainer.tsx
@@ -0,0 +1,26 @@
+import { useMemo } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+
+import { themeSettings } from "../../src/theme";
+import { useAppSelector } from "../../src/store/hook";
+
+export function TestContainer({ children }: { children: JSX.Element }) {
+  const themeMode = useAppSelector((state) => state.themeMode);
+  const theme = useMemo(
+    () => createTheme(themeSettings(themeMode)),
+    [themeMode]
+  );
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
--- client/cypress/utils/test-utils.tsx
+++ client/cypress/utils/test-utils.tsx
@@ -1,29 +1,11 @@
-import React, { PropsWithChildren } from "react";
-import { BrowserRouter } from "react-router-dom";
-// import { render } from '@testing-library/react'
-// import type { RenderOptions } from '@testing-library/react'
-// import { configureStore } from '@reduxjs/toolkit'
-// import type { PreloadedState } from '@reduxjs/toolkit'
+import { PropsWithChildren } from "react";
+
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material/styles";
-import { themeSettings } from "../../src/theme";
 
 import { setupStore } from "../../src/store/index";
-// import type { AppStore, RootState } from '../../src/store'
-// As a basic setup, import your same slice reducers
-// import userReducer from '../features/users/userSlice'
-
-// This type interface extends the default options for render from RTL, as well
-// as allows the user to specify other things such as initialState, store.
-// interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
-//   preloadedState?: PreloadedState<RootState>
-//   store?: AppStore
-// }
-
-const theme = createTheme(themeSettings("light"));
+import { TestContainer } from "./TestContainer";
 
 export function renderWithProviders({
   preloadedState = undefined,
@@ -31,16 +13,10 @@ export function renderWithProviders({
   store = setupStore(preloadedState),
 } = {}) {
   function Wrapper(children: PropsWithChildren<JSX.Element>): JSX.Element {
-    console.log("store", store);
     return (
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistStore(store)}>
-          <BrowserRouter>
-            <ThemeProvider theme={theme}>
-              <CssBaseline />
-              {children}
-            </ThemeProvider>
-          </BrowserRouter>
+          <TestContainer>{children}</TestContainer>
         </PersistGate>
       </Provider>
     );
